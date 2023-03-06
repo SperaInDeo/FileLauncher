cc57a5a625716f8547232f43ee5685f0ff178ca1
SperaInDeo <dustinmook@gmail.com>
2023-03-05 21:29:50 -0800

    First commit for GitHub. Unpolished Template

---
+++ README.md
@@ -0,0 +1,11 @@
+# File Launcher
+
+---
+03/05/2023
+
+I made this to make a template for a gui based application launcher. Launch games that aren't all on Steam. Launch websites. Open files. Do everything you can already do and less on the desktop.
+
+Also serves as the groundwork for a new project.
+
+-Enjoy
+*Dustin Mook*
--- fileSelect_GUI.py
+++ fileSelect_GUI.py
@@ -2,41 +2,59 @@ import os
 from tkinter import *
 from PIL import Image,ImageTk
 
-#def my_command():
-    #os.startfile(r"TestText.txt")
-num_rows = 5
-num_columns = 5
+num_rows = 1
+num_columns = 1
 bg_color = '#f0f0f0'
-image_file = 'Untitled.gif'
+image_file = 'TexasStar.ico'
+file_path = None
 
-def input_button_click():
-    entry_text = inputBox.get().replace('\\','\\\\')
+def entry_return(event):
+    outputButtonInitial.destroy()
+    entry_text = inputBox.get().replace('\\','\\\\').replace('\"', '')
+    with open('FileLauncher_file_list.csv', 'w', encoding='UTF-8') as update_file_doc:
+        update_file_doc.write(entry_text) 
     def execute_file():
-        os.startfile(entry_text)
-    # buttLabel1 = Label(root, text='button clicked', bg=bg_color)
-    # buttLabel1.grid(row=2, column=2)
+        try:
+            os.startfile(entry_text)
+        except:
+            print('file not found')
     global file_image
     file_image = ImageTk.PhotoImage(Image.open('TexasStar.ico'))
     outputButton = Button(root, text=entry_text, image=file_image,compound=TOP, state=NORMAL, command=execute_file, bd=1, bg=bg_color)
     outputButton.grid(row=3, column=0)
-    return image_file
+
+def input_add_button():
+    inputBox.grid(row=1, column=1)
+
+def execute_file_initial():
+    try:
+        os.startfile(file_path)
+    except:
+        print('file not found')
 
 root = Tk()
 root.title('File Launcher')
 root.iconbitmap('TexasStar.ico')
-root.geometry('800x600')
+root.geometry('400x400')
 root.configure(bg=bg_color)
 
 file_image = ImageTk.PhotoImage(Image.open(image_file))
+try:
+    with open('FileLauncher_file_list.csv', 'r', encoding='UTF-8') as read_file_doc:
+        file_path = read_file_doc.read()
+except:
+    print('There is currently no list file. Update the program to create file to launch program.')
 
-bannerLabel = Label(root, text='Hello world', bg=bg_color)
+if file_path != None:
+        outputButtonInitial = Button(root, text=file_path, image=file_image, compound=TOP, state=NORMAL, command=execute_file_initial, bd=1, bg=bg_color)
+        outputButtonInitial.grid(row=3, column=0)
+
+bannerLabel = Label(root, text='Welcome to the File Launcher', bg=bg_color)
 inputBox = Entry(root, text='input filepath here', bg=bg_color, borderwidth=5)
-#inputBox.bind('<Return>', input_button_click)
-inputButton = Button(root, text='Update File', state=NORMAL, padx=25, pady=25, command=input_button_click, bg=bg_color)
+inputBox.bind('<Return>', entry_return)
+inputButton = Button(root, text='Update File', state=NORMAL, padx=5, pady=5, command=input_add_button, bg=bg_color)
 
-bannerLabel.grid(row=0, column=(num_columns+1)//2)
-inputBox.grid(row=1, column=1)
+bannerLabel.grid(row=0, column=num_columns//2)
 inputButton.grid(row=1, column=0)
-
 
 root.mainloop()
